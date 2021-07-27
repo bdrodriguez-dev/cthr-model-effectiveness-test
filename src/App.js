90a359eda1c76cfaffac33209ff99f86b8a1f57a
Braulio Rodriguez <bdrodriguez.dev@gmail.com>
2021-07-27 19:30:59 -0400

    WIP: Comparing annotations to control dataset

--- src/App.js
+++ src/App.js
@@ -1,24 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Table from "react-bootstrap/Table";
+import Card from "react-bootstrap/Card";
 
 function App() {
+  const [annotatedRoofs, setAnnotatedRoofs] = useState([]);
+  const [trumbullData, setTrumbullData] = useState([]);
+
+  useEffect(() => {
+    async function fetchApi() {
+      try {
+        const response = await axios.get("http://localhost:8000/");
+        setAnnotatedRoofs(response.data.annotatedImages);
+        setTrumbullData(response.data.trumbullDataSet);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    fetchApi();
+  }, []);
+
+  const getAddress = (roof) => {
+    const filename = roof.filename;
+
+    return filename.replace(";", ".").replace(".png", "");
+  };
+
+  const getTown = (roof) => {
+    const filename = roof.filename;
+
+    return filename.replace(";", ".").replace(".png", "");
+  };
+
+  const getNumMissingShingles = (roof) => {
+    const numMissingShingles = roof.annotations.reduce(
+      (accumulator, currentValue) => {
+        if (currentValue.type === "missing-shingles") {
+          accumulator = accumulator + 1;
+        }
+        return accumulator;
+      },
+      0
+    );
+    return numMissingShingles;
+  };
+
+  const getNumPatches = (roof) => {
+    const numMissingShingles = roof.annotations.reduce(
+      (accumulator, currentValue) => {
+        if (currentValue.type === "patches") {
+          accumulator = accumulator + 1;
+        }
+        return accumulator;
+      },
+      0
+    );
+    return numMissingShingles;
+  };
+
+  const calculateAverageScore = (roof, damageType) => {
+    const damageScoreArray = roof.annotations
+      .filter((annotation) => {
+        return annotation.type === damageType;
+      })
+      .map((annotation) => {
+        return annotation.score;
+      });
+
+    if (damageScoreArray.length === 0) {
+      return 0;
+    }
+
+    const addScores = damageScoreArray.reduce((accumulator, currentValue) => {
+      return accumulator + parseFloat(currentValue);
+    }, 0);
+
+    const averageScore = addScores / damageScoreArray.length;
+    console.log(`${addScores} / ${damageScoreArray.length} = ${averageScore}`);
+    return averageScore.toFixed(2);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Card body style={{ width: "50rem", margin: "0 auto" }}>
+      <Table striped hover bordered>
+        <thead>
+          <tr>
+            <th>Address</th>
+            <th># Missing Shingles</th>
+            <th>Average Score</th>
+            <th># Patches</th>
+            <th>Average Score</th>
+          </tr>
+        </thead>
+        <tbody>
+          {annotatedRoofs.map((roof) => {
+            return (
+              <tr>
+                <td>{getAddress(roof)}</td>
+                <td>{getNumMissingShingles(roof)}</td>
+                <td>{calculateAverageScore(roof, "missing-shingles")}</td>
+                <td>{getNumPatches(roof)}</td>
+                <td>{calculateAverageScore(roof, "patches")}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+    </Card>
   );
 }
 
